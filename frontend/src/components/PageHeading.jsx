2e6df99bf17e67d2e0163a3f7de743d6b34771d5
adamcbark <adamcbark@gmail.com>
2024-01-24 06:32:01 -0500

    Stats component

--- frontend/src/components/PageHeading.jsx
+++ frontend/src/components/PageHeading.jsx
@@ -1,9 +1,9 @@
-export default function PageHeading({children}) {
+export default function PageHeading({title}) {
     return (
         <div className="md:flex md:items-center md:justify-between">
             <div className="min-w-0 flex-1">
                 <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight pb-4">
-                    {children}
+                    {title}
                 </h2>
             </div>
         </div>
--- frontend/src/screens/ProtectedScreen.jsx
+++ frontend/src/screens/ProtectedScreen.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import Card from '../components/Card'
 import PageHeading from '../components/PageHeading'
+import SectionHeading from '../components/SectionHeading'
+import Stats from '../components/Stats'
 
 function ProctectedScreen() {
     const userLogin = useSelector(state => state.userLogin)
@@ -23,8 +25,10 @@ function ProctectedScreen() {
             {userInfo && 
                 <Sidebar userInfo={userInfo}>
                     <Card>
-                        <PageHeading>Title of the Page</PageHeading>
+                        <PageHeading title="Page Title" />
+                        <SectionHeading title="Section Title" description="Blanditiis nostrum consectetur sint delectus culpa doloremque. Rerum tempore mollitia, provident laborum magni expedita corrupti obcaecati asperiores eum, voluptatibus assumenda autem quis." />
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis nostrum consectetur sint delectus culpa doloremque. Rerum tempore mollitia, provident laborum magni expedita corrupti obcaecati asperiores eum, voluptatibus assumenda autem quis.
+                        <Stats />
                     </Card>
                 </Sidebar>
             }
