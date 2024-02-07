8871fc6c7a633af32e44688b750ca056f92ce965
adamcbark <adamcbark@gmail.com>
2024-02-07 08:00:38 -0500

    Testing home

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 function HomeScreen() {
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!userInfo) {
+            navigate("/login")
+        } else {
+            navigate("/dashboard")
+        }
+    }, [navigate, userInfo])
+
     return (
         <>
             <div className="flex h-screen">
