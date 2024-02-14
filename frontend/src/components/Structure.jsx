35e83b7e8537cffc71aea236f49e3601d8688671
adamcbark <adamcbark@gmail.com>
2024-02-14 06:05:31 -0500

    Final structure diagram

--- frontend/src/components/Structure.jsx
+++ frontend/src/components/Structure.jsx
@@ -1,57 +1,26 @@
-import { OrganizationChart } from 'primereact/organizationchart';
-
 function Structure() {
-    const data = [{
-        label: 'Master',
-        expanded: true,
-        className: "px-8 py-4 rounded-lg border-8 border-blue-500 bg-blue-500 text-white w-full m-auto",
-        children: [
-            {
-                label: 'Key Metrics',
-                className: "px-8 py-4 rounded-lg border-8 border-emerald-500 bg-emerald-500 text-white w-full m-auto",
-                expanded: true,
-                children: [
-                    {
-                        label: 'MG Product',
-                        className: "px-8 py-4 rounded-lg border-8 border-red-500 bg-red-500 text-white w-full m-auto",
-                        expanded: true,
-                        children: [
-                            {
-                                label: 'Loss Data',
-                                className: "px-8 py-4 rounded-lg border-8 border-lime-500 bg-lime-500 text-white w-full m-auto",
-                            }
-                        ]
-                    },
-                    {
-                        label: 'HP 4 Hourly KPIs',
-                        className: "px-8 py-4 rounded-lg border-8 border-amber-500 bg-amber-500 text-white w-full m-auto",
-                        expanded: true,
-                        children: [
-                            {
-                                label: 'Loss Data',
-                                className: "px-8 py-4 rounded-lg border-8 border-lime-500 bg-lime-500 text-white w-full m-auto",
-                            }
-                        ]
-                    },
-                    {
-                        label: 'HP Hourly Count',
-                        expanded: true,
-                        className: "px-8 py-4 rounded-lg border-8 border-purple-500 bg-purple-500 text-white w-full m-auto",
-                        children: [
-                            {
-                                label: 'Loss Data',
-                                className: "px-8 py-4 rounded-lg border-8 border-lime-500 bg-lime-500 text-white w-full m-auto",
-                            }
-                        ]
-                    },
-                ]
-            }
-        ]
-    }];
-
     return (
         <>
-            <OrganizationChart value={data}></OrganizationChart>
+            <div className="flex flex-col border-2 border-gray-200 shadow-xl bg-gray-100 rounded-xl py-4 w-1/2 mx-auto text-center font-bold">
+                <div className="flex flex-row w-full bg-gray-200 py-2 pr-4">
+                    <div className="py-4 w-1/6">Tier 1</div>
+                    <div className="bg-blue-500 rounded-lg py-4 w-5/6 text-white">Key Metrics</div>
+                </div>
+
+                <div className="flex flex-row w-full bg-gray-100 py-2 pr-4">
+                    <div className="py-4 w-1/6">Tier 2</div>
+                    <div className="flex flex-row w-5/6 text-white space-x-4">
+                        <div className="bg-emerald-500 rounded-lg py-4 w-1/3">MG Product</div>
+                        <div className="bg-amber-500 rounded-lg py-4 w-1/3">HP 4 Hourly KPIs</div>
+                        <div className="bg-red-500 rounded-lg py-4 w-1/3">HP Hourly Count</div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row w-full py-2 bg-gray-200 pr-4">
+                    <div className="py-4 w-1/6">Tier 3</div>
+                    <div className="bg-pink-500 rounded-lg py-4 w-5/6 text-white">Loss Data</div>
+                </div>
+            </div>
         </>
     )
 }
