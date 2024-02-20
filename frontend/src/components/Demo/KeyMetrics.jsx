83089d293c95dab004a75e13b8b7e9aa50c5631f
adamcbark <adamcbark@gmail.com>
2024-02-20 07:55:10 -0500

    Demo page

--- frontend/src/components/Demo/KeyMetrics.jsx
+++ frontend/src/components/Demo/KeyMetrics.jsx
@@ -5,19 +5,25 @@ import {
 } from '@heroicons/react/24/outline'
 
 function KeyMetrics() {
-    const [productionDate, setProductionDate] = useState("")
-    const currentDate = new Date().toISOString().slice(0,10)
+    const [productionDate, setProductionDate] = useState(new Date())
+    const [displayDate, setDisplayDate] = useState("")
+    const datepicker = new Date().toISOString().slice(0,10)
+    const currentDate = new Date().toLocaleDateString("en-GB", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
     const subtractDate = (e) => {
-        setProductionDate(currentDate)
+        setProductionDate(productionDate - 1)
     }
 
     const addDate = (e) => {
+        setProductionDate(productionDate + 1)
+    }
+
+    const datePicker = (e) => {
         alert(e)
     }
 
     useEffect(() => {
-        setProductionDate(currentDate)
+        setDisplayDate(currentDate)
     }, [currentDate])
 
     return (
@@ -25,7 +31,7 @@ function KeyMetrics() {
             <div className="flex mb-4">
                 <div className="flex flex-col w-5/6 text-gray-900 space-x-2 space-y-2">
                     <div className="text-xs">Production Date</div>
-                    <div className="font-bold text-4xl">{productionDate}</div>
+                    <div className="font-bold text-4xl">{displayDate}</div>
                 </div>
             
                 <div className="flex flex-col xl:w-1/6 space-x-2 space-y-2">
@@ -40,8 +46,8 @@ function KeyMetrics() {
                         </button>
                         <input type="date" 
                             className="flex justify-center w-4/6 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" 
-                            defaultValue={productionDate}
-                            onChange={(e) => setProductionDate(e.target.value)}
+                            defaultValue={datepicker}
+                            onChange={datePicker}
                         />
                         <button
                             className="flex w-1/6 items-center justify-center rounded-r-md bg-blue-600  font-semibold text-white hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
