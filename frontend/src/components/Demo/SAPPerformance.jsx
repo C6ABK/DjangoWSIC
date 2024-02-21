a76a1deeee95a52d61f27c4bd0253d32a865e104
adamcbark <adamcbark@gmail.com>
2024-02-21 15:44:08 -0500

    SAP Performance demo

--- frontend/src/components/Demo/SAPPerformance.jsx
+++ frontend/src/components/Demo/SAPPerformance.jsx
@@ -1,14 +1,61 @@
+import Input from "./Input";
 import Tile from "./Tile";
 
 function SAPPerformance() {
   return (
     <Tile>
-      <div className="flex justify-between">
+      <div className="flex">
         <div className="font-bold text-gray-900 my-2 xl:text-sm">
           SAP Performance
         </div>
       </div>
-      <div></div>
+
+      <div className="space-y-0 xl:space-y-2">
+        <div className="flex flex-col w-full text-center">
+          <div className="flex text-xs  w-full px-4">
+            <div className="w-1/2">SAP Efficiency</div>
+            <div className="w-1/2">RTF Time Loss</div>
+          </div>
+          <div className="flex text-xs w-full px-4">
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full text-center">
+          <div className="flex text-xs  w-full px-4">
+            <div className="w-1/2">SAP Yield</div>
+            <div className="w-1/2">Damage Units</div>
+          </div>
+          <div className="flex text-xs w-full px-4">
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full text-center">
+          <div className="flex text-xs  w-full px-4">
+            <div className="w-1/2">SAP Damage</div>
+            <div className="w-1/2">Freshness Gaps</div>
+          </div>
+          <div className="flex text-xs w-full px-4">
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+            <div className="mx-4 w-1/2">
+              <Input default="0" />
+            </div>
+          </div>
+        </div>
+      </div>
     </Tile>
   );
 }
